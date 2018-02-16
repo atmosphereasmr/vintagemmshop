1e0eae52e8e3a9b7304fba6dfe424bb207e90fed
Logan <atmosphereasmr@gmail.com>
2018-02-16 11:48:02 -0800

    lets try this

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,24 @@ const app = express();
 
 app.use( express.static( `${__dirname}/../build` ) );
 
+app.get('/', function(request, response) {
+    console.log('Home page visited!');
+    const filePath = path.resolve(__dirname, './build', 'index.html');
+  
+    // read in the index.html file
+    fs.readFile(filePath, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      
+      // replace the special strings with server generated strings
+      data = data.replace(/\$OG_TITLE/g, 'Home Page');
+      data = data.replace(/\$OG_DESCRIPTION/g, "Home page description");
+      result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
+      response.send(result);
+    });
+  });
+
 const path = require('path')
 app.get('*', (req, res)=>{
  res.sendFile(path.join(__dirname, '../build/index.html'));
